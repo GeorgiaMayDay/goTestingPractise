e217ea6b813b5336fae6fa96dca412c7ee693d69
Georgia Cawley <georgia.cawley@bjss.com>
2022-10-21 15:31:30 +0100

    tests pass

--- shapes/shape.go
+++ shapes/shape.go
@@ -6,9 +6,9 @@ type Rectangle struct {
 }
 
 func Perimeter(r Rectangle) float64 {
-	return 0
+	return 2 * (r.Width + r.Height)
 }
 
 func Area(r Rectangle) float64 {
-	return 0
+	return r.Width * r.Height
 }
