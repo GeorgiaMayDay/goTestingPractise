7965a48e2b07619fb4debd60452666283f7f873d
Georgia Cawley <georgia.cawley@bjss.com>
2022-10-21 15:28:42 +0100

    Add shapes test on red. This is meant to let me work on structs and interfaces in go

---
+++ shapes/shape.go
@@ -0,0 +1,14 @@
+package shapes
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 0
+}
+
+func Area(r Rectangle) float64 {
+	return 0
+}
