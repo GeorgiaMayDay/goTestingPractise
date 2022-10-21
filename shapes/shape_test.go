0f752c3a3a3d8bbc539b516f27a3c360b8012e0c
Georgia Cawley <georgia.cawley@bjss.com>
2022-10-21 15:58:27 +0100

    Added Interfaces

--- shapes/shape_test.go
+++ shapes/shape_test.go
@@ -7,7 +7,7 @@ import (
 func TestPerimeter(t *testing.T) {
 	rect := Rectangle{10.0, 10.0}
 
-	got := Perimeter(rect)
+	got := rect.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -16,12 +16,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{12.0, 6.0}
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{12.0, 6.0}
 
-	got := Area(rect)
-	want := 72.0
+		got := rect.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
 }
