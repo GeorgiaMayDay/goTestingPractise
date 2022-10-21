6ad8aa3098734728bebb0074fd1c10cfee307637
Georgia Cawley <georgia.cawley@bjss.com>
2022-10-21 12:29:24 +0100

    iterator test now green

--- iteration/iterator.go
+++ iteration/iterator.go
@@ -1,5 +1,9 @@
 package iteration
 
-func Repeat(toBeRepeated string, timesRepeated int) string{
-	return ""
+func Repeat(toBeRepeated string, timesRepeated int) string {
+	var repeated string = ""
+	for i := 0; i < timesRepeated; i++ {
+		repeated = repeated + toBeRepeated
+	}
+	return repeated
 }
