97f609339f1d7ee81f642fdf595dc6b64693879f
Georgia Cawley <georgiamaycawley@gmail.com>
2022-10-24 12:14:43 +0100

    Set up tightly coupled function

---
+++ dependancy_injection/printing.go
@@ -0,0 +1,7 @@
+package printing
+
+import "fmt"
+
+func Greet(name string) {
+	fmt.Println("Hi ", name)
+}
---
+++ dependancy_injection/printing_test.go
@@ -0,0 +1,11 @@
+package printing
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	fmt.Println("Can't test Greet yet as it's too tightly coupled")
+	t.Fail()
+}
