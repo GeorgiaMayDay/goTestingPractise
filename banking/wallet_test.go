8cf63e030b8a554e73794449123d26338c10414e
Georgia Cawley <georgiamaycawley@gmail.com>
2022-10-24 11:12:14 +0100

    Refactor: added assertion function

--- banking/wallet_test.go
+++ banking/wallet_test.go
@@ -5,28 +5,31 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	wallet := Wallet{}
 
 	wallet.Deposit(Bitcoin(10))
 
 	t.Run("deposit", func(t *testing.T) {
-		got := wallet.Balance()
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("deposit", func(t *testing.T) {
 		wallet.Withdraw(5)
-
-		got := wallet.Balance()
 		want := Bitcoin(5)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 }
