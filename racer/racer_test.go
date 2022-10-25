dc82086ab687c3bdbd63d6a1948e2e7cb67120d8
Georgia Cawley <georgiamaycawley@gmail.com>
2022-10-25 10:13:11 +0100

    Website Racer work Examples of using http and testhttp in go

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -1,12 +1,28 @@
 package racer
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
+func makeMeAServer(delay time.Duration) (server *httptest.Server) {
+	newServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+	return newServer
+}
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := makeMeAServer(20 * time.Millisecond)
+	fastServer := makeMeAServer(2 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -14,4 +30,5 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
 }
--- racer/website_racer.go
+++ racer/website_racer.go
@@ -1,5 +1,25 @@
 package racer
 
+import (
+	"net/http"
+	"time"
+)
+
+func measureResponseTime(link string) time.Duration {
+	start := time.Now()
+	http.Get(link)
+	duration := time.Since(start)
+	return duration
+}
+
 func Racer(linkA string, linkB string) (winner string) {
-	return "Cheedle"
+	aDuration := measureResponseTime(linkA)
+
+	bDuration := measureResponseTime(linkB)
+
+	if bDuration < aDuration {
+		return linkB
+	} else {
+		return linkA
+	}
 }
