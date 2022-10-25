bf9882e8aa13d54589b5df6d1a8645301a02a45e
Georgia Cawley <georgiamaycawley@gmail.com>
2022-10-25 14:05:06 +0100

    Made counter concurrent safe with Mutex

--- counter/concurrent_counter.go
+++ counter/concurrent_counter.go
@@ -1,10 +1,15 @@
 package counter
 
+import "sync"
+
 type Counter struct {
 	count int
+	mu    sync.Mutex
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.count++
 }
 
--- counter/counter_test.go
+++ counter/counter_test.go
@@ -1,6 +1,9 @@
 package counter
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func assertCounter(t testing.TB, counter Counter, want int) {
 	t.Helper()
@@ -10,8 +13,31 @@ func assertCounter(t testing.TB, counter Counter, want int) {
 }
 
 func TestCounter(t *testing.T) {
-	counter := Counter{}
-	counter.Inc()
-	counter.Inc()
-	counter.Inc()
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+	})
+
+	t.Run("test concurrency", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		//Means all of the concurrent functions will have
+		// fired off by the time we get here
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+
+	})
 }
