acf9516791282b0f651257145209ff087397ec70
Georgia Cawley <georgiamaycawley@gmail.com>
2022-10-25 15:53:22 +0100

    Working through it in TDD design

--- romanNumerials/numerials_converter.go
+++ romanNumerials/numerials_converter.go
@@ -1,11 +1,31 @@
 package romanNumerials
 
-func ConvertToRomans(arabicNum int) string {
-	finalNum := ""
-	switch arabicNum {
-	case 1:
-		finalNum = "I"
+import "strings"
 
+func ConvertToRomans(arabicNum int) string {
+	var finalNum strings.Builder
+	for arabicNum > 0 {
+		switch arabicNum {
+		case 4:
+			finalNum.WriteString("IV")
+			arabicNum -= 4
+			break
+		case 9:
+			finalNum.WriteString("IX")
+			arabicNum -= 9
+			break
+		case 10:
+			finalNum.WriteString("X")
+			arabicNum -= 10
+			break
+		case 5, 6, 7, 8:
+			finalNum.WriteString("V")
+			arabicNum -= 5
+			break
+		default:
+			finalNum.WriteString("I")
+			arabicNum--
+		}
 	}
-	return finalNum
+	return finalNum.String()
 }
