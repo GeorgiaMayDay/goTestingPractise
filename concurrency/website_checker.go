1b02609af98c8779d941233a9fa0272bcecfb571
Georgia Cawley <georgiamaycawley@gmail.com>
2022-10-25 09:50:12 +0100

    got concurrency working new failing test for racer feature

--- concurrency/website_checker.go
+++ concurrency/website_checker.go
@@ -2,11 +2,26 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	webUrl  string
+	isValid bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChannel := make(chan result)
 
+	//Do the processing in parallel
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultsChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	//Unpack the channel into the map
+	for i := 0; i < len(urls); i++ {
+		r := <-resultsChannel
+		results[r.webUrl] = r.isValid
 	}
 
 	return results
